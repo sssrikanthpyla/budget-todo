cdfc0655ad0e92a8103eb7f333904cd58b9dc4e3
Satya Simhachala Srikanth Pyla <sssrikanth.pyla@gmail.com>
2024-07-07 11:11:14 +0530

    budget todo fastAPI configured

---
+++ budget-todo-fast-api-be/README.md
@@ -0,0 +1,6 @@
+- Install fast API
+- Install below deps
+    - pip install fastapi uvicorn sqlalchemy mysqlclient
+
+- To run mysql server
+    - docker exec -it <container_name_or_id> mysql -u <user> -p
---
+++ budget-todo-fast-api-be/main.py
@@ -0,0 +1,21 @@
+
+import uvicorn
+from fastapi import FastAPI
+from app.routers.item import router
+from app.database.database_configure import engine, Base
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+# FastAPI app instance
+app = FastAPI()
+
+# Include the routers
+app.include_router(router)
+
+@app.get("/")
+async def home():
+    return "Welcome to home"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
