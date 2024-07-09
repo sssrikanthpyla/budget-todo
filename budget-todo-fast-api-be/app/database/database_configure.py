f560997fc00e30ce319bf774302a6f805afe2682
Satya Simhachala Srikanth Pyla <sssrikanth.pyla@gmail.com>
2024-07-09 17:31:27 +0530

    Conneted mysql database

--- budget-todo-fast-api-be/app/database/database_configure.py
+++ budget-todo-fast-api-be/app/database/database_configure.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:rootpassword@localhost:3306/mydatabase"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://myuser:myuser@127.0.0.1:3306/mydatabase"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
